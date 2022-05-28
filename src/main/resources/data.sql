b5e9d80ecfe7732c1879c3fd1abfadd1ef3423a1
hyoghurt <ilfat.hafizov2@yandex.ru>
2022-05-28 14:32:59 +0300

    add data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO student(name, age, time_from, time_to, course) VALUES ('Mick', 23, 14, 15, 'java');
+INSERT INTO student(name, age, time_from, time_to, course) VALUES ('Lick', 13, 15, 16, 'java');
+INSERT INTO student(name, age, time_from, time_to, course) VALUES ('Pick', 22, 12, 13, 'python');
+INSERT INTO student(name, age, time_from, time_to, course) VALUES ('Nick', 10, 14, 19, 'python');
+
+INSERT INTO course VALUES ('java', 'java course');
+INSERT INTO course VALUES ('python', 'python course');
